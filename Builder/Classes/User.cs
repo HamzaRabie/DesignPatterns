ad10c3474e764d65cf377eec77d85331d37fc006
hamza <hamza@DESKTOP-FG6AAAJ>
2026-05-21 12:56:33 +0200

    apply builder pattern

---
+++ Builder/Classes/User.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder.Classes
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool TwoFactorEnabled { get; set; }
+
+        public User(UserBuilder builder)
+        {
+            Name = builder.Name;
+            Email = builder.Email;
+            IsAdmin = builder.IsAdmin;
+            TwoFactorEnabled = builder.TwoFactorEnabled;
+        }
+    }
+
+    public class UserBuilder
+    {
+        public string Name { get; }
+        public string Email { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool TwoFactorEnabled { get; set; }
+
+        public UserBuilder(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public UserBuilder SetEmail(string email)
+        {
+            this.Email = email;
+            return this;
+        }
+
+        public UserBuilder SetAdmin(bool isAdmin)
+        {
+            IsAdmin = isAdmin;
+            return this;
+        }
+
+        public UserBuilder SetTwoFactorAuth(bool TwoFactorEnabled)
+        {
+            this.TwoFactorEnabled = TwoFactorEnabled;
+            return this;
+        }
+
+        public User Build()
+        {
+            return new User(this);
+        }
+    }
+}
---
+++ Builder/Program.cs
@@ -0,0 +1,17 @@
+﻿using Builder.Classes;
+
+namespace Builder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new UserBuilder("Hamza").SetEmail("Hamza@gmail.com")
+                                                .SetTwoFactorAuth(true)
+                                                .SetAdmin(false)
+                                                .Build();
+
+            Console.WriteLine($"User With Name {user.Name} , Email : {user.Email}");
+        }
+    }
+}
